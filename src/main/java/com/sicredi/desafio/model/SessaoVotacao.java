1866702c9f337f9d10ba7043a9b817242f055978
Thales Birino <thales.birino@gmail.com>
2023-05-25 18:06:52 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/sicredi/desafio/model/SessaoVotacao.java
@@ -0,0 +1,28 @@
+package com.sicredi.desafio.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SessaoVotacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "pauta_id")
+    private Pauta pauta;
+
+    private LocalDateTime abertura;
+    private LocalDateTime encerramento;
+
+}
---
+++ src/main/java/com/sicredi/desafio/model/Voto.java
@@ -0,0 +1,25 @@
+package com.sicredi.desafio.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Voto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pauta_id")
+    private Pauta pauta;
+
+    private String associadoId;
+    private boolean voto;
+
+}
